ef2b6fd1d2f00d3d57f1c5f5bf4b71409169f891
JinEunPark <wlsdmsrnfl@naver.com>
2023-08-03 21:23:02 +0900

    this is end

---
+++ jineun/script/hambuger.js
@@ -0,0 +1,6 @@
+function onMenuClick() {
+	let navbar = document.getElementById("person");
+	console.log(navbar);
+	let responsive_class_name = "responsive";
+	navbar.classList.toggle("responsive");
+}
